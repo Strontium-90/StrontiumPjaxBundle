5d59794c408647bb98d7c3d7a0353d84dfa4cace
Strontium-90 <edgar.li.2007@gmail.com>
2014-10-06 07:46:04 +0400

    Ещё тесты

--- Tests/Twig/PjaxExtensionTest.php
+++ Tests/Twig/PjaxExtensionTest.php
@@ -13,11 +13,12 @@ class PjaxExtensionTest extends \Twig_Test_IntegrationTestCase
             ->getMockBuilder('Strontium\PjaxBundle\VersionGenerator\AuthTokenGenerator')
             ->disableOriginalConstructor()
             ->getMock();
-
-        $pjax->setVersionGenerator($generator);
         $generator
             ->method('generate')
             ->will($this->onConsecutiveCalls(1, 2, null, 3));
+
+        $pjax->setVersionGenerator($generator);
+
         return array(
             new PjaxExtension($pjax),
         );
