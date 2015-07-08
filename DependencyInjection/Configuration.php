94efdaff05409588971f6bee21d26d845fcccb00
Aleksey Bannov <Aleksey.Bannov@noveogroup.com>
2015-07-08 15:37:02 +0300

    composer.json

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -2,15 +2,11 @@
 
 namespace Strontium\PjaxBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Builder\NodeBuilder;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 /**
- * This is the class that validates and merges configuration from your app/config files
- *
- * To learn more see {@link
- * http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ * {@inheritdoc}
  */
 class Configuration implements ConfigurationInterface
 {
