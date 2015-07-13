5116a2697a15d590d3061b94f354a0bdf4fc71a6
Strontium-90 <a.s.bannov@gmail.com>
2015-07-13 17:20:02 +0300

    phpspec/prophecy-phpunit fix

--- Tests/EventListener/KernelResponseListenerTest.php
+++ Tests/EventListener/KernelResponseListenerTest.php
@@ -1,10 +1,35 @@
 <?php
 namespace Strontium\PjaxBundle\Tests\EventListener;
 
+use Prophecy\PhpUnit\ProphecyTestCase;
+use Prophecy\Prophecy\ObjectProphecy;
+use Strontium\PjaxBundle\EventListener\KernelResponseListener;
+use Strontium\PjaxBundle\PjaxInterface;
+
 class KernelResponseListenerTest extends ProphecyTestCase
 {
+    /**
+     * @var PjaxInterface|ObjectProphecy
+     */
+    protected $pjax;
+
+    /**
+     * @var KernelResponseListener
+     */
+    protected $listener;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->pjax = $this->prophesize('\Strontium\PjaxBundle\PjaxInterface');
+        $this->listener = new KernelResponseListener($this->pjax->reveal());
+    }
+
+
     public function test_it_should_add_pjax_version_to_response()
     {
-
     }
 }
