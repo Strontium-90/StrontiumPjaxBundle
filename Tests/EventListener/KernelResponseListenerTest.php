d24b041552669423b2e85139ec40297ae83dd663
Strontium-90 <a.s.bannov@gmail.com>
2015-07-13 17:04:56 +0300

    phpspec/prophecy-phpunit fix

---
+++ Tests/EventListener/KernelResponseListenerTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace Strontium\PjaxBundle\Tests\EventListener;
+
+class KernelResponseListenerTest extends ProphecyTestCase
+{
+    public function test_it_should_add_pjax_version_to_response()
+    {
+
+    }
+}
