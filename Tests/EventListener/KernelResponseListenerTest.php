eb7a72bd6523a34d4ae2f99e5e3e3202be099c8e
Aleksey Bannov <Aleksey.Bannov@noveogroup.com>
2015-07-14 17:24:24 +0300

    tests fixes

--- Tests/EventListener/KernelResponseListenerTest.php
+++ Tests/EventListener/KernelResponseListenerTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Strontium\PjaxBundle\Tests\EventListener;
 
+use Prophecy\Argument;
 use Prophecy\PhpUnit\ProphecyTestCase;
 use Prophecy\Prophecy\ObjectProphecy;
 use Strontium\PjaxBundle\EventListener\KernelResponseListener;
@@ -35,7 +36,7 @@ class KernelResponseListenerTest extends ProphecyTestCase
         $event = $this->prophesize('Symfony\Component\HttpKernel\Event\FilterResponseEvent');
         $response = $this->prophesize('Symfony\Component\HttpFoundation\Response');
         $request = $this->prophesize('\Symfony\Component\HttpFoundation\Request');
-        $headers = $this->prophesize('\Symfony\Component\HttpFoundation\ParameterBag');
+        $headers = $this->prophesize('\Symfony\Component\HttpFoundation\ResponseHeaderBag');
         $response->headers = $headers->reveal();
 
         $event->getRequest()->willReturn($request->reveal());
@@ -48,5 +49,49 @@ class KernelResponseListenerTest extends ProphecyTestCase
         $headers->set('X-PJAX-Version', 'version')->shouldBeCalled();
 
         $this->listener->addPjaxVersion($event->reveal());
+    }
+
+    public function test_it_should_skip_if_its_not_pjax_redirect()
+    {
+        $event = $this->prophesize('Symfony\Component\HttpKernel\Event\FilterResponseEvent');
+
+        $request = $this->prophesize('\Symfony\Component\HttpFoundation\Request');
+        $requestHeaders = $this->prophesize('\Symfony\Component\HttpFoundation\HeaderBag');
+        $request->headers = $requestHeaders->reveal();
+        $response = $this->prophesize('Symfony\Component\HttpFoundation\Response');
+        $headers = $this->prophesize('\Symfony\Component\HttpFoundation\ResponseHeaderBag');
+        $response->headers = $headers->reveal();
+        $event->getRequest()->willReturn($request->reveal());
+        $event->getResponse()->willReturn($response->reveal());
+
+
+        $this->pjax->isPjaxRequest($request->reveal())->willReturn(false);
+        $headers->setCookie()->shouldNotBeCalled();
+
+
+        $this->listener->pjaxRedirect($event->reveal());
+    }
+
+    public function test_it_should_add_cookie_on_redirect()
+    {
+        $event = $this->prophesize('Symfony\Component\HttpKernel\Event\FilterResponseEvent');
+
+        $request = $this->prophesize('\Symfony\Component\HttpFoundation\Request');
+        $requestHeaders = $this->prophesize('\Symfony\Component\HttpFoundation\HeaderBag');
+        $request->headers = $requestHeaders->reveal();
+        $response = $this->prophesize('Symfony\Component\HttpFoundation\Response');
+        $headers = $this->prophesize('\Symfony\Component\HttpFoundation\ResponseHeaderBag');
+        $response->headers = $headers->reveal();
+        $event->getRequest()->willReturn($request->reveal());
+        $event->getResponse()->willReturn($response->reveal());
+
+
+        $this->pjax->isPjaxRequest($request->reveal())->willReturn(true);
+        $response->isRedirect()->wilLReturn(true);
+
+        $headers->get('Location')->willReturn('redirect_uri');
+        $headers->setCookie(Argument::any())->shouldBeCalled();
+
+        $this->listener->pjaxRedirect($event->reveal());
     }
 }
